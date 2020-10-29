d182be5f728e6948d8b3c6ec6dbe1a370b3d870f
sidharsha <siddharth.landers@gmail.com>
2020-10-28 22:41:03 -0400

    Postman screen Snippets for error handling as required

--- server.js
+++ server.js
@@ -55,7 +55,9 @@ app.post('/budget',(req,res)=>{
                 console.log("Connection to the database is established");
                 budgetModel.insertMany(data,(err,data)=>{
                     if(err){
-                        console.log(err);                        
+                        console.log(err);      
+                        res.send(err);
+                        mongoose.connection.close();
                     }else{
                         console.log("insert successful"); 
                         res.send(data);    
